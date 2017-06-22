7005d4013b0423bff9c43d42847834a2da1f6387
luke pate <lukewpate@gmail.com>
2017-06-21 21:49:25 -0400

    add json faces

--- App/Containers/TextablesScreen.js
+++ App/Containers/TextablesScreen.js
@@ -17,33 +17,7 @@ class ListviewSectionsExample extends React.Component {
     * This is an array of objects with the properties you desire
     * Usually this should come from Redux mapStateToProps
     *************************************************************/
-    const dataObjects = {
-      first: [
-        {title: 'First Title', description: 'First Description'},
-        {title: 'Second Title', description: 'Second Description'},
-        {title: 'Third Title', description: 'Third Description'},
-        {title: 'Fourth Title', description: 'Fourth Description'},
-        {title: 'Fifth Title', description: 'Fifth Description'},
-        {title: 'Sixth Title', description: 'Sixth Description'},
-        {title: 'Seventh Title', description: 'Seventh Description'},
-        {title: 'Eighth Title', description: 'Eighth Description'},
-        {title: 'Ninth Title', description: 'Ninth Description'},
-        {title: 'Tenth Title', description: 'Tenth Description'}
-      ],
-      second: [
-        {title: 'Eleventh Title', description: 'Eleventh Description'},
-        {title: '12th Title', description: '12th Description'},
-        {title: '13th Title', description: '13th Description'},
-        {title: '14th Title', description: '14th Description'},
-        {title: '15th Title', description: '15th Description'},
-        {title: '16th Title', description: '16th Description'},
-        {title: '17th Title', description: '17th Description'},
-        {title: '18th Title', description: '18th Description'},
-        {title: '19th Title', description: '19th Description'},
-        {title: '20th Title', description: '20th Description'},
-        {title: 'BLACKJACK!', description: 'BLACKJACK! Description'}
-      ]
-    }
+    const dataObjects = require('../Fixtures/face.json')
     /* ***********************************************************
     * STEP 2
     * Teach datasource how to detect if rows are different
@@ -76,8 +50,8 @@ class ListviewSectionsExample extends React.Component {
     // in different sections
     return (
       <View style={styles.row}>
-        <Text style={styles.boldLabel}>Section {sectionID} - {rowData.title}</Text>
-        <Text style={styles.label}>{rowData.description}</Text>
+        <Text style={styles.boldLabel}>{rowData.name}</Text>
+        <Text style={styles.label}>{rowData.art}</Text>
       </View>
     )
   }
